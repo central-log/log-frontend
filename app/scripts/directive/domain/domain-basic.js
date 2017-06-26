d227fc8799fc92cb99fb477b8da9815e95692c5c
benlv <benlv.hu@dianrong.com>
2017-06-26 15:25:19 +0800

    Refactor Domain Basic

--- app/scripts/directive/domain/domain-basic.js
+++ app/scripts/directive/domain/domain-basic.js
@@ -1,12 +1,52 @@
-define(function () {
-  function fn() {
+define(['utils/Constant','utils/Utils'],function (Constant, Utils) {
+  function fn(DomainSvc, ngDialog) {
     return {
       restrict: 'E',
       scope: {
-        detail: '='
+        envDetails: '=',
+        domainId: '='
       },
       templateUrl: 'views/directive/domain/basic.html',
-      link: function (scope) {
+      link: function ($scope) {
+        $scope.logLevels = Constant.logLevels;
+
+        $scope.confirmEnvAdd = function () {
+              $scope.domainEnvEntity.domainId = $scope.domainId;
+              $scope.domainEnvEntity.beforeName = $scope.envDetails.name;
+              $scope.envSubmiting = true;
+
+              DomainSvc.modifyEnvDomain($scope.domainEnvEntity, function (resp) {
+                $scope.envSubmiting = false;
+                if (resp.errMsg) {
+                  $scope.addEnvErrorMsg = resp.errMsg ? resp.errMsg : Constant.createError;
+                  return;
+                }
+                $scope.addEnvErrorMsg = '';
+                $scope.addEnvInstanceDialog.close();
+                window.location.reload();
+              }, function (resp) {
+                $scope.envSubmiting = false;
+                $scope.addEnvErrorMsg = resp.errMsg ? resp.errMsg : Constant.createError;
+              });
+        }
+
+        $scope.showAddEnvDialog = function(isModify){
+          $scope.isModify = isModify;
+
+          $scope.domainEnvEntity = {
+            logLevel: $scope.envDetails.logLevel,
+            description: $scope.envDetails.description,
+            email: $scope.envDetails.email,
+            name: $scope.envDetails.name
+          };
+
+          $scope.addEnvErrorMsg = '';
+          $scope.addEnvInstanceDialog = ngDialog.open({
+            template: './views/domain/domain-env-add.html',
+            className: 'ngdialog-custom-default',
+            scope: $scope
+          });
+        }
 
       }
     };
@@ -14,6 +54,6 @@ define(function () {
 
   return {
     name: 'domainBasic',
-    directiveFn: [fn]
+    directiveFn: ['DomainSvc', 'ngDialog', fn]
   };
 });
