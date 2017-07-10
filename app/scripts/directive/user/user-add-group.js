6d4495df9bef940cd0a36b313355e61895f16388
benlv <benlv.hu@dianrong.com>
2017-07-10 15:23:52 +0800

    User Detail Tabs

---
+++ app/scripts/directive/user/user-add-group.js
@@ -0,0 +1,102 @@
+'use strict';
+define(['utils/Constant'], function (Constant) {
+    function fn($location, RoleSvc, PermissionSvc) {
+        return {
+            restrict: 'E',
+            scope: {
+                id: '='
+            },
+            templateUrl: 'views/directive/user/user-add-group.html',
+            link: function ($scope) {
+
+                function buildDisplayData() {
+                    $scope.users.forEach(function (p) {
+                        if (!(p.type in $scope.displayData)) {
+                            $scope.displayData[p.type] = [p];
+                        } else {
+                            $scope.displayData[p.type].push(p);
+                        }
+                    });
+                    $scope.types = Object.keys($scope.displayData);
+
+                    $scope.addedPermissionList.forEach(function (p) {
+                        var target = $scope.displayData[p.type];
+
+                        target.find(function (e) {
+                            if (e.id === p.id) {
+                                e.added = true;
+                            }
+                        });
+                    });
+                    console.log($scope.displayData);
+                }
+                $scope.criteria = {
+                    name: ''
+                };
+            // Event listeners
+                $scope.lastCritria = null;
+                $scope.getPermissions = function () {
+                    $scope.displayData = {};
+                    $scope.types = [];
+                    var searchCriteria = {
+                        name: $scope.criteria.name ? $scope.criteria.name : null
+                    };
+
+                    $scope.lastCritria = searchCriteria;
+                    $scope.loadingStatus = Constant.loading;
+                    $scope.users = [];
+                    PermissionSvc.getAllPermission(searchCriteria, function (resp) {
+                        $scope.users = resp;
+
+                        if (!$scope.users.length) {
+                            $scope.loadingStatus = Constant.loadEmpty;
+                            return;
+                        }
+
+                        RoleSvc.getRolePermission({ roleId: $scope.id },
+                          function (addedPermissionList) {
+                              $scope.loadingStatus = '';
+                              $scope.addedPermissionList = addedPermissionList;
+
+                              if (!addedPermissionList || !addedPermissionList.length) {
+                                  $scope.addedPermissionList = [];
+                              }
+
+                              buildDisplayData();
+
+                          }, function () {
+                              $scope.loadingStatus = '';
+                              $scope.addedPermissionList = [];
+                              buildDisplayData();
+                          });
+
+                    }, function () {
+                        $scope.loadingStatus = Constant.loadError;
+                    });
+                };
+
+                $scope.addPermission = function (type, index, id) {
+                    RoleSvc.addPermission({ roleId: $scope.id, permissionId: id }, function () {
+                        $scope.displayData[type][index].added = 'added';
+                        console.log($scope.displayData);
+                    }, function (error) {
+                        var resp = (error && error.data) || {};
+                        var msg = resp.errMsg ? resp.errMsg : Constant.operateError;
+
+                        alert(msg);
+                    });
+                };
+                $scope.$watch('id', function () {
+                    if ($scope.id) {
+                        $scope.getPermissions();
+                    }
+                });
+            }
+        };
+    }
+
+    return {
+        name: 'userAddGroup',
+        directiveFn: ['$location', 'RoleSvc', 'PermissionSvc', fn]
+    };
+});
---
+++ app/scripts/directive/user/user-permission.js
@@ -0,0 +1,85 @@
+'use strict';
+define(['utils/Constant'], function (Constant) {
+    function fn($location, RoleSvc, ngDialog) {
+        return {
+            restrict: 'E',
+            scope: {
+                id: '='
+            },
+            templateUrl: 'views/directive/user/user-permission.html',
+            link: function ($scope) {
+
+            // Event listeners
+                $scope.lastCritria = null;
+                $scope.queryUser = function () {
+                    var searchCriteria = {
+                        roleId: $scope.id
+                    };
+
+                    $scope.displayData = {};
+                    $scope.types = [];
+
+                    $scope.lastCritria = searchCriteria;
+                    $scope.loadingStatus = Constant.loading;
+                    $scope.users = [];
+                    RoleSvc.getRolePermission(searchCriteria, function (resp) {
+                        $scope.loadingStatus = '';
+                        $scope.users = resp;
+
+                        if (!resp || !resp.length) {
+                            $scope.users = [];
+                            $scope.loadingStatus = Constant.loadEmpty;
+                        }
+
+                        $scope.users.forEach(function (p) {
+                            if (!(p.type in $scope.displayData)) {
+                                $scope.displayData[p.type] = [p];
+                            } else {
+                                $scope.displayData[p.type].push(p);
+                            }
+                        });
+                        $scope.types = Object.keys($scope.displayData);
+
+                    }, function () {
+                        $scope.loadingStatus = Constant.loadError;
+                    });
+                };
+
+                $scope.deletePermission = function (id) {
+                    $scope.submiting = true;
+                    RoleSvc.deletePermission({ roleId: $scope.id, permissionId: id }, function () {
+                        $scope.submiting = false;
+                        $scope.deleteInstanceDialog.close();
+                        $scope.queryUser();
+                    }, function (error) {
+                        $scope.submiting = false;
+                        var resp = (error && error.data) || {};
+
+                        $scope.submitErrorMsg = resp.errMsg ? resp.errMsg : Constant.operateError;
+                    });
+                };
+
+                $scope.openDeleteDialog = function (p) {
+                    $scope.submitErrorMsg = '';
+                    $scope.selectedPermission = p;
+                    $scope.deleteInstanceDialog = ngDialog.open({
+                        template: './views/directive/role/permission-delete.html',
+                        className: 'ngdialog-custom-default',
+                        scope: $scope
+                    });
+                };
+
+                $scope.$watch('id', function () {
+                    if ($scope.id) {
+                        $scope.queryUser();
+                    }
+                });
+            }
+        };
+    }
+
+    return {
+        name: 'userPermission',
+        directiveFn: ['$location', 'RoleSvc', 'ngDialog', fn]
+    };
+});
